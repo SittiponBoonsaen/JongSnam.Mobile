00e574a8f412899807986eac1239b50a09179c16
fastfivesadd <sittipon46160@gmail.com>
2021-02-12 00:47:54 +0700

    doing validation

--- JongSnam.Mobile/JongSnam.Mobile/ViewModels/AddStoreViewModel.cs
+++ JongSnam.Mobile/JongSnam.Mobile/ViewModels/AddStoreViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JongSnam.Mobile.Validations;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xamarin.Forms;
@@ -7,20 +8,47 @@ namespace JongSnam.Mobile.ViewModels
 {
     public class AddStoreViewModel : BaseViewModel
     {
+
         public Command SaveCommand { get; }
 
         public AddStoreViewModel()
         {
             SaveCommand = new Command(OnAlertYesNoClicked);
         }
+
+        public ValidatableObject<string> Image { get; set; }
+        public ValidatableObject<string> Name { get; set; }
+        public ValidatableObject<string> Address { get; set; }
+        public ValidatableObject<string> SubDistrict { get; set; }
+        public ValidatableObject<string> District { get; set; }
+        public ValidatableObject<string> Province { get; set; }
+        public ValidatableObject<string> ContactMobile { get; set; }
+        public ValidatableObject<string> Lat { get; set; }
+        public ValidatableObject<string> Long { get; set; }
+        public ValidatableObject<string> OfficeHours { get; set; }
+        public ValidatableObject<string> Rules { get; set; }
+
         public void OnAppearing()
         {
             IsBusy = true;
         }
+        private void InitValidation()
+        {
+            Image = new ValidatableObject<string>();
+            Image.Validations.Add(new IsNullOrEmptyRule<string> {ValidationMessage = "Image is null"});
+        }
+        private bool IsValid
+        {
+            get
+            {
+                return Image.Validate();
+            }
+        }
+
         async void OnAlertYesNoClicked()
         {
             bool answer = await Shell.Current.DisplayAlert("Question?", "Would you like to play a game", "Yes", "No");
-            if (!answer)
+            if (!answer || !IsValid)
             {
                 return;
             }
