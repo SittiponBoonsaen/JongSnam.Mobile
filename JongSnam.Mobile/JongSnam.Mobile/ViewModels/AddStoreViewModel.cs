ee45bb22f1e0f676cbd8f8e2f4e1860d932883af
fastfivesadd <sittipon46160@gmail.com>
2021-02-12 00:16:33 +0700

    add field , edt field

---
+++ JongSnam.Mobile/JongSnam.Mobile/ViewModels/AddStoreViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace JongSnam.Mobile.ViewModels
+{
+    public class AddStoreViewModel : BaseViewModel
+    {
+        public Command SaveCommand { get; }
+
+        public AddStoreViewModel()
+        {
+            SaveCommand = new Command(OnAlertYesNoClicked);
+        }
+        public void OnAppearing()
+        {
+            IsBusy = true;
+        }
+        async void OnAlertYesNoClicked()
+        {
+            bool answer = await Shell.Current.DisplayAlert("Question?", "Would you like to play a game", "Yes", "No");
+            if (!answer)
+            {
+                return;
+            }
+
+            await Shell.Current.GoToAsync("..");
+        }
+
+    }
+}
--- JongSnam.Mobile/JongSnam.Mobile/Views/AddStorePage.xaml.cs
+++ JongSnam.Mobile/JongSnam.Mobile/Views/AddStorePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JongSnam.Mobile.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,16 @@ namespace JongSnam.Mobile.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AddStorePage : ContentPage
     {
+        AddStoreViewModel _viewModel;
         public AddStorePage()
         {
             InitializeComponent();
+            BindingContext = _viewModel = new AddStoreViewModel();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
         }
     }
 }
