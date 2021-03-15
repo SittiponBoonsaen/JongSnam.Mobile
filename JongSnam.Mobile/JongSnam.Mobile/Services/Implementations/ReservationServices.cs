da394ce63089278f02dca355df5eb2543e0ffb8f
fastfivesadd <sittipon46160@gmail.com>
2021-03-15 21:39:11 +0700

    15/3

--- JongSnam.Mobile/JongSnam.Mobile/Services/Implementations/ReservationServices.cs
+++ JongSnam.Mobile/JongSnam.Mobile/Services/Implementations/ReservationServices.cs
@@ -4,11 +4,25 @@ using System.Text;
 using System.Threading.Tasks;
 using JongSnam.Mobile.Services.Base;
 using JongSnam.Mobile.Services.Interfaces;
+using JongSnamService.Models;
+using Xamarin.Forms;
 
 namespace JongSnam.Mobile.Services.Implementations
 {
     public class ReservationServices : BaseServices, IReservationServices
     {
+        private readonly IMapperService _mapperService;
+
+        public ReservationServices()
+        {
+            _mapperService = DependencyService.Get<IMapperService>();
+        }
+        public async Task<IEnumerable<FieldDtoBasePagingDto>> GetYourReservation(int storeId, int ownerId, int currentPage, int pageSize)
+        {
+            var response = await JongSnamServices.GetYourReservationWithHttpMessagesAsync(storeId, ownerId, currentPage, pageSize);
+
+            return await GetRespondDtoHandlerHttpStatus<IEnumerable<FieldDtoBasePagingDto>>(response);
+        }
 
     }
 }
--- JongSnam.Mobile/JongSnam.Mobile/Services/Interfaces/IReservationServices.cs
+++ JongSnam.Mobile/JongSnam.Mobile/Services/Interfaces/IReservationServices.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using JongSnamService.Models;
 
 namespace JongSnam.Mobile.Services.Interfaces
 {
     public interface IReservationServices
     {
+        Task<IEnumerable<FieldDtoBasePagingDto>> GetYourReservation(int storeId, int ownerId, int currentPage, int pageSize);
+
 
     }
 }
