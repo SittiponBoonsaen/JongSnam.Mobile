2ad47123f3001a005edfaaaa2c5d62cae8f38032
fastfivesadd <sittipon46160@gmail.com>
2021-03-20 00:27:47 +0700

    items commit

--- JongSnam.Mobile/JongSnam.Mobile/ViewModels/ItemsViewModel.cs
+++ JongSnam.Mobile/JongSnam.Mobile/ViewModels/ItemsViewModel.cs
@@ -5,6 +5,7 @@ using JongSnamService.Models;
 using System;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.IO;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
@@ -14,12 +15,48 @@ namespace JongSnam.Mobile.ViewModels
     {
         private readonly IStoreServices _storeServices;
 
-        private Item _selectedItem;
+        private StoreDto _selectedItem;
+        private string _imageBase64;
+        private Xamarin.Forms.ImageSource _image;
 
         public ObservableCollection<StoreDto> Items { get; }
         public Command LoadItemsCommand { get; }
         public Command AddItemCommand { get; }
-        public Command<Item> ItemTapped { get; }
+        public Command<StoreDto> ItemTapped { get; }
+
+        public StoreDto SelectedItem
+        {
+            get => _selectedItem;
+            set
+            {
+                SetProperty(ref _selectedItem, value);
+                OnItemSelected(value);
+            }
+        }
+        public string ImageBase64
+        {
+            get { return _imageBase64; }
+            set
+            {
+                _imageBase64 = value;
+                OnPropertyChanged("ImageBase64");
+                Image = Xamarin.Forms.ImageSource.FromStream(
+                    () => new MemoryStream(Convert.FromBase64String(_imageBase64)));
+            }
+        }
+
+        public Xamarin.Forms.ImageSource Image
+        {
+            get { return _image; }
+            set
+            {
+                _image = value;
+                OnPropertyChanged("Image");
+            }
+        }
+
+
+
 
         public ItemsViewModel()
         {
@@ -31,7 +68,7 @@ namespace JongSnam.Mobile.ViewModels
 
             LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
 
-            ItemTapped = new Command<Item>(OnItemSelected);
+            ItemTapped = new Command<StoreDto>(OnItemSelected);
 
             AddItemCommand = new Command(OnAddItem);
         }
@@ -44,10 +81,14 @@ namespace JongSnam.Mobile.ViewModels
             {
                 Items.Clear();
                 var items = await _storeServices.GetStores(1, 10);
+
+
                 foreach (var item in items)
                 {
                     Items.Add(item);
+                    Image = item.Image;
                 }
+
             }
             catch (Exception ex)
             {
@@ -65,28 +106,19 @@ namespace JongSnam.Mobile.ViewModels
             SelectedItem = null;
         }
 
-        public Item SelectedItem
-        {
-            get => _selectedItem;
-            set
-            {
-                SetProperty(ref _selectedItem, value);
-                OnItemSelected(value);
-            }
-        }
-
         private async void OnAddItem(object obj)
         {
             await Shell.Current.GoToAsync(nameof(NewItemPage));
         }
 
-        async void OnItemSelected(Item item)
+        async void OnItemSelected(StoreDto storeDto)
         {
-            if (item == null)
+            if (storeDto == null)
+            {
                 return;
-
+            }
             // This will push the ItemDetailPage onto the navigation stack
-            await Shell.Current.GoToAsync($"{nameof(ItemDetailPage)}?{nameof(ItemDetailViewModel.ItemId)}={item.Id}");
+            await Shell.Current.GoToAsync($"{nameof(ItemDetailPage)}?{nameof(ItemDetailViewModel.ItemId)}={storeDto.Id}");
         }
     }
 }
