be60048e4b8a97839f046ce2413196dd8292c57a
Perawat <peejenpq@gmail.com>
2021-03-24 01:07:08 +0700

    23/3

--- JongSnam.Mobile/JongSnam.Mobile/Autorests/JongSnam.Services/JongSnamServices.cs
+++ JongSnam.Mobile/JongSnam.Mobile/Autorests/JongSnam.Services/JongSnamServices.cs
@@ -3377,7 +3377,7 @@ namespace JongSnamService
                 _responseContent = await _httpResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
                 try
                 {
-                    _result.Body = SafeJsonConvert.DeserializeObject<ReservationDtoBasePagingDto>(_responseContent, DeserializationSettings);
+                    _result.Body = SafeJsonConvert.DeserializeObject<ReservationDetailDtoBasePagingDto>(_responseContent, DeserializationSettings);
                 }
                 catch (JsonException ex)
                 {
