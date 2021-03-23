ffc985c22bf453b882819c5490a988c24c0ec8c8
fastfivesadd <sittipon46160@gmail.com>
2021-03-24 01:01:23 +0700

    23/3

---
+++ JongSnam.Mobile/JongSnam.Mobile/Views/DetailYourReservationPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JongSnam.Mobile.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace JongSnam.Mobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetailYourReservationPage : ContentPage
+    {
+        DetailYourReservationViewModel _viewModel;
+        public DetailYourReservationPage(int reservationId)
+        {
+            InitializeComponent();
+            BindingContext = _viewModel = new DetailYourReservationViewModel(reservationId);
+        }
+    }
+}
