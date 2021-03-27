a16425e9d82209c83d8e589dcbd97e01a520c627
Perawat <peejenpq@gmail.com>
2021-03-28 04:36:40 +0700

    update field

--- JongSnam.Mobile/JongSnam.Mobile/ViewModels/FieldViewModel.cs
+++ JongSnam.Mobile/JongSnam.Mobile/ViewModels/FieldViewModel.cs
@@ -1,5 +1,9 @@
-﻿using JongSnam.Mobile.Services.Interfaces;
+﻿using System;
+using System.Threading.Tasks;
+using JongSnam.Mobile.Constants;
+using JongSnam.Mobile.Services.Interfaces;
 using JongSnamService.Models;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace JongSnam.Mobile.ViewModels
@@ -8,15 +12,97 @@ namespace JongSnam.Mobile.ViewModels
     {
         private readonly IReservationServices _reservationServices;
 
+        private double _fromTime;
+        private double _toTime;
+        private DateTime _selectedDate = DateTime.Now;
+
+        public Command BookCommand { get; private set; }
+
+        public double FromTime
+        {
+            get
+            {
+                return _fromTime;
+            }
+
+            set
+            {
+                _fromTime = value;
+                OnPropertyChanged(nameof(FromTime));
+            }
+        }
+
+        public double ToTime
+        {
+            get
+            {
+                return _toTime;
+            }
+
+            set
+            {
+                _toTime = value;
+                OnPropertyChanged(nameof(ToTime));
+            }
+        }
+
+        public DateTime SelectedDate
+        {
+            get
+            {
+                return _selectedDate;
+            }
+
+            set
+            {
+                _selectedDate = value;
+                OnPropertyChanged(nameof(SelectedDate));
+            }
+        }
+
         public FieldViewModel(FieldDto fieldDto)
         {
             _reservationServices = DependencyService.Get<IReservationServices>();
+
+            BookCommand = new Command(async () => await ExecuteBookCommand());
 
         }
 
         internal void OnAppearing()
         {
             IsBusy = true;
+        }
+
+        async Task ExecuteBookCommand()
+        {
+            try
+            {
+                IsBusy = true;
+                var startHour = Math.Truncate(FromTime);
+                var stopHour = Math.Truncate(ToTime);
+                var startTime = ToTime - Math.Truncate(FromTime);
+                var stopTime = ToTime - Math.Truncate(ToTime);
+                var userId = Preferences.Get(AuthorizeConstants.UserIdKey, string.Empty);
+
+                var request = new ReservationRequest
+                {
+                    StoreId = 1,
+                    UserId = Convert.ToInt32(userId),
+                    FieldId = 1,
+                    StartTime = new DateTime(SelectedDate.Year, SelectedDate.Month, SelectedDate.Day, (int)startHour, (int)startTime, 0),
+                    StopTime = new DateTime(SelectedDate.Year, SelectedDate.Month, SelectedDate.Day, (int)stopHour, (int)stopTime, 0),
+                };
+
+                var aa = await _reservationServices.CreateReservation(request);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                IsBusy = false;
+            }
         }
     }
 }
