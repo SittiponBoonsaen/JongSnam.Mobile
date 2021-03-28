8e1b9c2b0046d6b8066d198d2bb6637b78f5d77d
fastfivesadd <sittipon46160@gmail.com>
2021-03-28 19:38:52 +0700

    28/3

--- JongSnam.Mobile/JongSnam.Mobile/ViewModels/FieldViewModel.cs
+++ JongSnam.Mobile/JongSnam.Mobile/ViewModels/FieldViewModel.cs
@@ -11,10 +11,18 @@ namespace JongSnam.Mobile.ViewModels
     public class FieldViewModel : BaseViewModel
     {
         private readonly IReservationServices _reservationServices;
+
+        private readonly IFieldServices _fieldServices;
         
         private TimeSpan _fromTime;
         private TimeSpan _toTime;
         private DateTime _selectedDate = DateTime.Now;
+        private string _nameField;
+        private string _sizeField;
+        private string _price;
+        private string _isOpen;
+        private string _storeName;
+
         public Command BookCommand { get; private set; }
 
         public TimeSpan FromTime
@@ -59,12 +67,83 @@ namespace JongSnam.Mobile.ViewModels
             }
         }
 
-        public FieldViewModel(FieldDto fieldDto)
+        public string NameField
+        {
+            get
+            {
+                return _nameField;
+            }
+
+            set
+            {
+                _nameField = value;
+                OnPropertyChanged(nameof(NameField));
+            }
+        }
+        public string SizeField
+        {
+            get
+            {
+                return _sizeField;
+            }
+
+            set
+            {
+                _sizeField = value;
+                OnPropertyChanged(nameof(SizeField));
+            }
+        }
+
+        public string Price
+        {
+            get
+            {
+                return _price;
+            }
+
+            set
+            {
+                _price = value;
+                OnPropertyChanged(nameof(Price));
+            }
+        }
+        public string IsOpen
+        {
+            get
+            {
+                return _isOpen;
+            }
+
+            set
+            {
+                _isOpen = value;
+                OnPropertyChanged(nameof(IsOpen));
+            }
+        }
+        public string StoreName
+        {
+            get
+            {
+                return _storeName;
+            }
+
+            set
+            {
+                _storeName = value;
+                OnPropertyChanged(nameof(StoreName));
+            }
+        }
+
+
+
+    public FieldViewModel(FieldDto fieldDto, string StoreName)
         {
             _reservationServices = DependencyService.Get<IReservationServices>();
+            _fieldServices = DependencyService.Get<IFieldServices>();
 
             BookCommand = new Command(async () => await ExecuteBookCommand(fieldDto));
 
+            Task.Run(async () => await ExecuteLoadItemsCommand(fieldDto, StoreName));
         }
 
         internal void OnAppearing()
@@ -72,11 +151,36 @@ namespace JongSnam.Mobile.ViewModels
             IsBusy = true;
         }
 
+        async Task ExecuteLoadItemsCommand(FieldDto fieldDto, string storeName)
+        {
+            IsBusy = true;
+            try
+            {
+                var data = await _fieldServices.GetFieldById(fieldDto.Id.Value);
+
+                NameField = data.Name;
+                Price = data.Price.Value.ToString() + " /ชม";
+                IsOpen = data.IsOpen == true ? IsOpen = "เปิดบริการ" : IsOpen = "ปิดบริการ";
+                SizeField = data.Size;
+                StoreName = storeName;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
         async Task ExecuteBookCommand(FieldDto fieldDto)
         {
             try
             {
                 //IsBusy = true;
+
+
                 var userId = Preferences.Get(AuthorizeConstants.UserIdKey, string.Empty);
                 var storeId = Preferences.Get(AuthorizeConstants.StoreIdKey, string.Empty);
 
@@ -89,9 +193,17 @@ namespace JongSnam.Mobile.ViewModels
                     StopTime = new DateTime(SelectedDate.Year, SelectedDate.Month, SelectedDate.Day, ToTime.Hours, ToTime.Minutes, 0),
                 };
 
-                var result = await _reservationServices.CreateReservation(request);
+                var start = request.StartTime.Value.ToString("MM/dd/yyyy h:mm tt");
+                var stop = request.StopTime.Value.ToString("MM/dd/yyyy h:mm tt");
 
-                if (result)
+                bool answer = await Shell.Current.DisplayAlert("แจ้งเตือน!", $"\n ชื่อร้านที่จอง: {StoreName} \n ชื่อสนามที่จอง: {NameField} \n ราคา : {Price} \n ขนาดของสนาม : {SizeField} \n ตั้งแต่ : {start} \n " +
+                    $"ถึง : {stop}\n" , "ใช่", "ไม่");
+                if (!answer)
+                {
+                    return;
+                }
+                var result = await _reservationServices.CreateReservation(request);
+                if(result)
                 {
                     await Shell.Current.Navigation.PopAsync();
                 }
