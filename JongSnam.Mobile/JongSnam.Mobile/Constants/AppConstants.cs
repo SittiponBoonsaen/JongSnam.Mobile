c06da7f599229ab94c6393e914cc8268cf59c71c
Perawat <peejenpq@gmail.com>
2021-04-03 14:12:46 +0700

    add app constants

---
+++ JongSnam.Mobile/JongSnam.Mobile/Constants/AppConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JongSnam.Mobile.Constants
+{
+    public static class AppConstants
+    {
+        public static string MapApiKey = "AIzaSyBK_uiPTr_IZnJta_KNw9EZvHOSAYzcMQ4";
+    }
+}
